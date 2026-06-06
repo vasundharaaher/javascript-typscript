7a7d805c1dd834cb2f057eeaf832a1c3bd6294eb
vasundharaaher <122102502+vasundharaaher@users.noreply.github.com>
2026-06-06 11:09:06 +0530

    Add files via upload

---
+++ JavaScript2/function.js
@@ -0,0 +1,72 @@
+// function myfunction(){
+//     console.log("H")
+//      console.log("E")
+//       console.log("L")
+//        console.log("L")
+//         console.log("O")
+// }
+
+
+// myfunction()
+
+//++++++++++++++++++++++++++++++++++
+
+// function myfunction(nm1 , nm2){
+//     console.log(nm1+nm2)  
+//     // return nm1+nm2  
+// }
+
+// const res = myfunction(2,4)
+// console.log(res);
+
+// myfunction(2,"4")
+// myfunction(2,"a")
+// myfunction(2,null)
+
+////////////////////////////////////////////////////
+
+// function loginfn(usernm){
+//     if(!usernm){
+//         return `${usernm} hello !!!`
+//     }
+//     return "please provide usernm"
+// }
+// console.log(loginfn());
+ 
+
+//////////////////////////////////////////////////////
+
+// function calculateCart(vl1 , vl2 , ...num1){
+//     return (num1)
+// }
+// console.log(calculateCart(200,300,400,500,600))
+
+
+//////////////////////////////////////////////////////
+
+// const user = {
+//     name: "vasu",
+//     age: 24,
+//     email: "vasu@gmail.com",
+//     location: "Chh. sambhajinagar",
+//     isLoggedin: false,
+//     lastLogin : ["Monday","Saturday"],
+   
+// }
+// function infouser(details){
+//     return `Hello dear "${details.name}" with email "${details.email}"`
+// }
+// res = infouser(user)
+// console.log(res)
+
+//////////////////////////////////////////////////////
+
+// const arr =  ["Monday","Saturday","Sunday","Tuesday"]
+// function arrfun(getarr){
+//     return getarr[1]
+// }
+
+// console.log(arrfun(arr));
+
+//////////////////////////////////////////////////////
+
