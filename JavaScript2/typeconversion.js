f4526b6d4ef4af819ca54131c305224936a2b58d
vasundhara <vasundharaaher55@gmail.com>
2026-07-01 14:57:49 +0530

    saved local code

--- JavaScript2/typeconversion.js
+++
@@ -1,4 +0,0 @@
-let num = ""
-let numcon = Number(num)
-console.log(typeof numcon)
-console.log(numcon)
--- Typscript/main.ts
+++
@@ -1,4 +0,0 @@
-import {add, subtract } from './second-typescript.js';
-
-console.log(add(5,10));
-console.log(subtract(5,3));
